c57046299b50346c6102483a769002b7f0977b7f
Anzormumladze <anzor.anzor.mumladze@gmail.com>
2020-03-22 13:02:03 +0400

    git init

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Georgian Cuisine</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
